f13696e0c7d592af6834a1317e442ceef82b6e49
w4dd325 <w4dd325@gmail.com>
2021-10-03 15:45:38 +0100

    updates

--- MongoDB_Example/Program.cs
+++ MongoDB_Example/Program.cs
@@ -10,12 +10,32 @@ namespace MongoDB_Example
         static void Main(string[] args)
         {
             //NOTES:
-            //Cluster name = myFirstDatabase
+            //-------------------------------------------------------------------------------------------------------------------------------------------------------------------------
+            //My DB details
+            //-------------------------------------------------------------------------------------------------------------------------------------------------------------------------
+            //Wadders_88 | d7BlSqKCqqfCHY4Z | mysampledb-shard-00-02.xcrwt.mongodb.net:27017
+            //Cluster name = mySampleDB
             //Database name(s) = sample_airbnb | sample_analytics | sample_geospatial | sample_mflix | sample_restaurants | sample_supplies | sample_training | sample_weatherdata
             //Collections (aka tables) = listed under each database ^
-
+            //Connection string =
+            //var settings = MongoClientSettings.FromConnectionString("mongodb+srv://Wadders_88:O2z4HTSyLDrecSeo@mysampledb.xcrwt.mongodb.net/mysampledb?retryWrites=true&w=majority");
+            //
+            //-------------------------------------------------------------------------------------------------------------------------------------------------------------------------
+            //Tom's DB details
+            //-------------------------------------------------------------------------------------------------------------------------------------------------------------------------
+            //Wadders_88 | d7BlSqKCqqfCHY4Z | sampledb-shard-00-02.egd8v.mongodb.net:27017
+            //Cluster name = sampledb
+            //Database name(s) = sample_airbnb | sample_analytics | sample_geospatial | sample_mflix | sample_restaurants | sample_supplies | sample_training | sample_weatherdata
+            //Collections (aka tables) = listed under each database ^
+            //Connection string =
+            //var settings = MongoClientSettings.FromConnectionString("mongodb+srv://Wadders_88:d7BlSqKCqqfCHY4Z@sampledb.egd8v.mongodb.net/sampledb?retryWrites=true&w=majority");
+            //
+            //-------------------------------------------------------------------------------------------------------------------------------------------------------------------------
+            //
+            //
+            
             //MongoDB connection
-            var settings = MongoClientSettings.FromConnectionString("mongodb+srv://Wadders_88:O2z4HTSyLDrecSeo@mysampledb.xcrwt.mongodb.net/myFirstDatabase?retryWrites=true&w=majority");
+            var settings = MongoClientSettings.FromConnectionString("mongodb+srv://Wadders_88:d7BlSqKCqqfCHY4Z@sampledb.egd8v.mongodb.net/sampledb?retryWrites=true&w=majority");
             var client = new MongoClient(settings);
             var database = client.GetDatabase("sample_training");
 
